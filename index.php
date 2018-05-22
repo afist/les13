c4d417ea9a344a495f845275741faef35ad4dfd2
afist <barahuda@ua.fm>
2018-05-22 11:47:51 +0300

    1.0.2

--- index.php
+++ index.php
@@ -24,73 +24,96 @@ $conn->close();
 
 
 
-$conn1 = new mysqli($serverName, $userName, $password, $dbName);
+$conn = new mysqli($serverName, $userName, $password, $dbName);
 
-$sql1 = "CREATE TABLE `{$dbName}`.`{$tableName}` ( 
+$sqlCreatTable = "CREATE TABLE `{$dbName}`.`{$tableName}` ( 
         `id` INT NOT NULL AUTO_INCREMENT ,
         `numberHouse` VARCHAR(20) NOT NULL ,
         `street` VARCHAR(20) NOT NULL ,
         `town` VARCHAR(20) NOT NULL ,
          PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;";
 
-$sql1.= "CREATE TABLE `{$dbName}`.`{$table2Name}` ( 
+$sqlCreatTable.= "CREATE TABLE `{$dbName}`.`{$table2Name}` ( 
         `id` INT NOT NULL AUTO_INCREMENT , 
         `name` VARCHAR(30) NOT NULL , 
         `soname` VARCHAR(30) NOT NULL , 
         `apartmentNumber` INT(4) NOT NULL , 
-        `idHouse` INT NOT NULL , 
-         PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci";
+        `idHouse` INT NOT NULL ,
+        `idSocialNetwork` INT NOT NULL ,
+         PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;";
 
-$sql1.= "CREATE TABLE `{$dbName}`.`{$table2Name}` ( 
+$sqlCreatTable.= "CREATE TABLE `{$dbName}`.`{$table3Name}` ( 
         `id` INT NOT NULL AUTO_INCREMENT , 
-        `name` VARCHAR(30) NOT NULL , 
-        `soname` VARCHAR(30) NOT NULL , 
-        `apartmentNumber` INT(4) NOT NULL , 
-        `idHouse` INT NOT NULL , 
-         PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci";
+        `facebook` VARCHAR(30) NOT NULL , 
+        `vk` VARCHAR(30) NOT NULL , 
+        `instagram` VARCHAR(30) NOT NULL , 
+        `idUsers` INT NOT NULL , 
+         PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;";
 
 
-if ($conn1->multi_query($sql1)===true) {
+if ($conn->multi_query($sqlCreatTable)===true) {
     echo "table created<br>";
 } else {
     echo $conn1->error;
 }
-$conn1->close();
+$conn->close();
 
 
-$conn2 = new mysqli($serverName, $userName, $password, $dbName);
 
- $sql2.="ALTER TABLE `people` ADD FOREIGN KEY (`idHouse`) REFERENCES `house`(`id`);";
-if ($conn2->multi_query($sql2)===true) {
+
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+
+ $sqlPrimaryKey="ALTER TABLE `{$table2Name}` ADD FOREIGN KEY (`idHouse`) REFERENCES `{$tableName}`(`id`);";
+ $sqlPrimaryKey.="ALTER TABLE `{$table3Name}` ADD FOREIGN KEY (`idUsers`) REFERENCES `{$table2Name}`(`id`);";
+ $sqlPrimaryKey.="ALTER TABLE `{$table2Name}` ADD FOREIGN KEY (`idSocialNetwork`) REFERENCES `{$table3Name}`(`id`);";
+
+if ($conn->multi_query($sqlPrimaryKey)===true) {
     echo "user created1<br>";
 } else {
-    echo "errr".$conn2->error;
+    echo "errr".$conn->error;
 }
-$conn2->close();
+$conn->close();
 
 
-$conn2 = new mysqli($serverName, $userName, $password, $dbName);
-$sql2 = "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('57', 'Бажана', 'Киев');";
-$sql2.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('1', 'Бажана', 'Киев');";
-$sql2.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('7', 'Бажана', 'Киев');";
-$sql2.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('5', 'Бажана', 'Киев');";
-$sql2.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('8', 'Бажана', 'Киев');";
-if ($conn2->multi_query($sql2)===true) {
+
+
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+$sqlDataRecording = "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('57', 'Бажана', 'Киев');";
+$sqlDataRecording.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('1', 'Бажана', 'Киев');";
+$sqlDataRecording.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('7', 'Бажана', 'Киев');";
+$sqlDataRecording.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('5', 'Бажана', 'Киев');";
+$sqlDataRecording.= "INSERT INTO {$tableName} (numberHouse, street, town) VALUES('8', 'Бажана', 'Киев');";
+if ($conn->multi_query($sqlDataRecording)===true) {
     echo "house created<br>";
 } else {
-    echo $conn2->error;
+    echo $conn->error;
 }
-$conn2->close();
+$conn->close();
 
-$conn2 = new mysqli($serverName, $userName, $password, $dbName);
-$sql2 = "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse) VALUES('Vasa', 'big', '24' , '1');";
-$sql2.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse) VALUES('Vasa', 'big', '1' , '2');";
-$sql2.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse) VALUES('Vasa', 'big', '2' , '3');";
-$sql2.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse) VALUES('Vasa', 'big', '23' , '1');";
-$sql2.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse) VALUES('Vasa', 'big', '24' , '1');";
-if ($conn2->multi_query($sql2)===true) {
+
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+$sqlDataRecording = "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse, idSocialNetwork) VALUES('Vasa', 'big', '24' , '1', '1');";
+$sqlDataRecording.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse, idSocialNetwork) VALUES('Vasa', 'big', '1' , '2', '2');";
+$sqlDataRecording.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse, idSocialNetwork) VALUES('Vasa', 'big', '2' , '3', '3');";
+$sqlDataRecording.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse, idSocialNetwork) VALUES('Vasa', 'big', '23' , '1', '4');";
+$sqlDataRecording.= "INSERT INTO {$table2Name} (name, soname, apartmentNumber, idHouse, idSocialNetwork) VALUES('Vasa', 'big', '24' , '1', '5');";
+if ($conn->multi_query($sqlDataRecording)===true) {
     echo "people created<br>";
 } else {
-    echo $conn2->error;
+    echo $conn->error;
 }
-$conn2->close();
+$conn->close();
+
+
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+$sqlDataRecording = "INSERT INTO {$table3Name} (facebook, vk, instagram, idUsers) VALUES('facebook.com/vita-ak0', 'vk.com/vita0', 'intaBlog1' , '1');";
+$sqlDataRecording.= "INSERT INTO {$table3Name} (facebook, vk, instagram, idUsers) VALUES('facebook.com/vita-ak1', 'vk.com/vita1', 'intaBlog2' , '2');";
+$sqlDataRecording.= "INSERT INTO {$table3Name} (facebook, vk, instagram, idUsers) VALUES('facebook.com/vita-ak2', 'vk.com/vita2', 'intaBlog3' , '3');";
+$sqlDataRecording.= "INSERT INTO {$table3Name} (facebook, vk, instagram, idUsers) VALUES('facebook.com/vita-ak3', 'vk.com/vita3', 'intaBlog4' , '4');";
+$sqlDataRecording.= "INSERT INTO {$table3Name} (facebook, vk, instagram, idUsers) VALUES('facebook.com/vita-ak4', 'vk.com/vita4', 'intaBlog5' , '5');";
+if ($conn->multi_query($sqlDataRecording)===true) {
+    echo "people created<br>";
+} else {
+    echo $conn->error;
+}
+$conn->close();
